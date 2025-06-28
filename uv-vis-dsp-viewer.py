7a7daf885a0a6143a22437917d92e6440ac25d99
Fontan030 <pomanfedurin@gmail.com>
2025-06-28 21:27:21 +0700

    Add language selector and settings window

--- uv-vis-dsp-viewer.py
+++ uv-vis-dsp-viewer.py
@@ -1,5 +1,6 @@
 import chardet
 import gettext
+import json
 import os
 import matplotlib.pyplot as plt
 import tkinter as tk
@@ -8,9 +9,11 @@ from tkinter import filedialog, ttk
 
 #import sys
 
-uv_vis_border = 500 # nm
-uv_default_conc = [5, -5] # 5e-5 mol/L
-vis_default_conc = [1, -2] # 1e-2 mol/L
+lang_dict = {
+'en': 'English',
+'kk': 'Қазақша',
+'ru': 'Русский'
+}
 
 def load_file():
     global sample_name, wavelength_list, data_list, peaks_list
@@ -37,15 +40,15 @@ def parse_dsp_string(dsp_string):
     start_wavelength = int(dsp_lines[meta_inf_idx+1])
     end_wavelength = int(dsp_lines[meta_inf_idx+2])
     step = int(dsp_lines[meta_inf_idx+3])
-    if (start_wavelength + end_wavelength) / 2 < uv_vis_border:
+    if (start_wavelength + end_wavelength) / 2 < config['uv_vis_border']:
         spectrum_type = _('UV')
-        conc_signif_stringvar.set(uv_default_conc[0])
-        conc_exp_stringvar.set(uv_default_conc[1])
+        conc_signif_stringvar.set(config['uv_default_conc'][0])
+        conc_exp_stringvar.set(config['uv_default_conc'][1])
     else:
         spectrum_type = _('visible')
-        conc_signif_stringvar.set(vis_default_conc[0])
-        conc_exp_stringvar.set(vis_default_conc[1])
-    meta_inf_str = f'{sample_filename}: {spectrum_type},\nstart {start_wavelength}, end {end_wavelength}, step {step}'
+        conc_signif_stringvar.set(config['vis_default_conc'][0])
+        conc_exp_stringvar.set(config['vis_default_conc'][1])
+    meta_inf_str = f'{sample_filename}:\n{spectrum_type} ({start_wavelength}-{end_wavelength} {nm_label}), step {step} {nm_label}'
     meta_inf_msg.config(text=meta_inf_str)
     data_idx = dsp_lines.index('#DATA')
     data_list = dsp_lines[data_idx+1:]
@@ -149,40 +152,134 @@ def save_plot_as(out_format):
     if output_filename:
         fig.savefig(output_filename)
 
+def select_language(lang_code):
+    config['lang'] = lang_code
+    write_config_file()
+    t_new = gettext.translation(domain='messages', localedir='locale', languages=[lang_code], fallback=True)
+    restart_alert = t_new.gettext('Restart application to switch language!')
+    extinction_text_stringvar.set(restart_alert)
+
+def show_settings():
+    global uv_vis_entry, uv_entry_1, uv_entry_2, vis_entry_1, vis_entry_2, settings_window
+    settings_window = tk.Toplevel()
+    settings_window.title(_('Settings'))
+    settings_window.geometry('350x250')
+    root.eval(f'tk::PlaceWindow {str(settings_window)} center')
+    settings_window.columnconfigure(index=0, weight=5)
+    for c in range(1,5): settings_window.columnconfigure(index=c, weight=1)
+    for r in range(6): settings_window.rowconfigure(index=r, weight=1)
+    uv_vis_label_1 = tk.Label(settings_window, text=_('UV-vis border'))
+    uv_vis_label_1.grid(row=0, column=0, sticky='w', padx = 10)
+    uv_vis_entry = ttk.Entry(settings_window, width=4)
+    uv_vis_entry.grid(row=0, column=3)
+    uv_vis_entry.insert(0, config['uv_vis_border'])
+    uv_vis_label_2 = tk.Label(settings_window, text=nm_label)
+    uv_vis_label_2.grid(row=0, column=4, sticky='w', padx = 10)
+    default_conc_label = tk.Label(settings_window, text=_('Default sample concentrations:'))
+    default_conc_label.grid(row=1, column=0, columnspan=5, sticky='w', padx = 10)
+    uv_label_1 = tk.Label(settings_window, text=_('UV'))
+    uv_label_1.grid(row=2, column=0, sticky='w', padx = 10)
+    uv_entry_1 = ttk.Entry(settings_window, width=4)
+    uv_entry_1.grid(row=2, column=1)
+    uv_entry_1.insert(0, config['uv_default_conc'][0])
+    uv_label_2 = tk.Label(settings_window, text='• 10^')
+    uv_label_2.grid(row=2, column=2)
+    uv_entry_2 = ttk.Entry(settings_window, width=3)
+    uv_entry_2.grid(row=2, column=3)
+    uv_entry_2.insert(0, config['uv_default_conc'][1])
+    uv_label_3 = tk.Label(settings_window, text=_('mol/L'))
+    uv_label_3.grid(row=2, column=4, sticky='w')
+    vis_label_1 = tk.Label(settings_window, text=_('visible'))
+    vis_label_1.grid(row=3, column=0, sticky='w', padx = 10)
+    vis_entry_1 = ttk.Entry(settings_window, width=4)
+    vis_entry_1.grid(row=3, column=1)
+    vis_entry_1.insert(0, config['vis_default_conc'][0])
+    vis_label_2 = tk.Label(settings_window, text='• 10^')
+    vis_label_2.grid(row=3, column=2)
+    vis_entry_2 = ttk.Entry(settings_window, width=3)
+    vis_entry_2.grid(row=3, column=3)
+    vis_entry_2.insert(0, config['vis_default_conc'][1])
+    vis_label_3 = tk.Label(settings_window, text=_('mol/L'))
+    vis_label_3.grid(row=3, column=4, sticky='w')
+    reset_btn = tk.Button(settings_window, text='Reset', command=lambda: set_config('default', True) )
+    reset_btn.grid(row=5, column=0, sticky='w', padx = 10)
+    ok_btn = tk.Button(settings_window, text='OK', command=lambda: set_config('custom', True) )
+    ok_btn.grid(row=5, column=4, sticky='e', padx = 10)
+
+def set_config(config_type, close_window):
+    global config
+    if config_type == 'default':
+        config = {
+            'lang':             'en',
+            'uv_vis_border':    500,     # nm
+            'uv_default_conc':  [5, -5], # 5e-5 mol/L
+            'vis_default_conc': [1, -2]  # 1e-2 mol/L
+        }
+    elif config_type == 'custom':
+        config['uv_vis_border'] = int(uv_vis_entry.get())
+        config['uv_default_conc'][0] = float(uv_entry_1.get())
+        config['uv_default_conc'][1] = int(uv_entry_2.get())
+        config['vis_default_conc'][0] = float(vis_entry_1.get())
+        config['vis_default_conc'][1] = int(vis_entry_2.get())
+    write_config_file()
+    if close_window:
+        settings_window.destroy()
+
 def show_help():
     help_window = tk.Toplevel()
     help_window.title(_('Help'))
-    help_window.geometry("300x200")
+    help_window.geometry('300x200')
     root.eval(f'tk::PlaceWindow {str(help_window)} center')
     help_text = 'UV-vis DSP viewer 0.1\n\nAn open source viewer for UV-vis spectral data files generated by VISIONlite Scan\n\nCreated with matplotlib and tkinter\n\nhttps://github.com/Fontan030'
     tk.Message(help_window, text=help_text, justify='center', width=288).pack(pady=20)
+
+def write_config_file():
+    with open('uvdv_config.json', 'w') as settings_file:
+        json.dump(config, settings_file)
 
 def close_all_windows():
     plt.close()
     root.destroy()
 
-t = gettext.translation(domain='messages', localedir='locale', languages=['kk'], fallback=True)  
+if os.path.exists('uvdv_config.json'):
+    with open('uvdv_config.json', 'r') as settings_file:
+        config = json.load(settings_file)
+else:
+    print('New config file')
+    set_config('default', False)
+
+t = gettext.translation(domain='messages', localedir='locale', languages=[config['lang']], fallback=True)
 _ = t.gettext
 nm_label = _('nm')
 
 root = tk.Tk()
-root.title("UV-vis DSP viewer")
-root.geometry("400x400")
+root.title('UV-vis DSP viewer')
+root.geometry('450x400')
 root.eval('tk::PlaceWindow . center')
-root.protocol("WM_DELETE_WINDOW", close_all_windows)
+root.protocol('WM_DELETE_WINDOW', close_all_windows)
 
-root.option_add("*tearOff", tk.FALSE)
+root.option_add('*tearOff', tk.FALSE)
 file_menu = tk.Menu()
 file_menu.add_command(label=_('Open'), command=load_file)
 save_as_menu = tk.Menu()
-save_as_menu.add_command(label="PNG", command=lambda: save_plot_as('.png') )
-save_as_menu.add_command(label="PDF", command=lambda: save_plot_as('.pdf') )
-save_as_menu.add_command(label="SVG", command=lambda: save_plot_as('.svg') )
+save_as_menu.add_command(label='PNG', command=lambda: save_plot_as('.png') )
+save_as_menu.add_command(label='PDF', command=lambda: save_plot_as('.pdf') )
+save_as_menu.add_command(label='SVG', command=lambda: save_plot_as('.svg') )
 file_menu.add_cascade(label=_('Save plot as'), menu=save_as_menu, state=tk.DISABLED)
+
+edit_menu = tk.Menu()
+lang_menu = tk.Menu()
+for lang_code, lang_name in lang_dict.items():
+    lang_menu.add_command(label=lang_name, command=lambda lc=lang_code: select_language(lc))
+edit_menu.add_cascade(label=_('Language'), menu=lang_menu)
+edit_menu.add_command(label=_('Settings'), command=show_settings)
+
 help_menu = tk.Menu()
 help_menu.add_command(label=_('About'), command=show_help)
+
 main_menu = tk.Menu()
 main_menu.add_cascade(label=_('File'), menu=file_menu)
+main_menu.add_cascade(label=_('Edit'), menu=edit_menu)
 main_menu.add_cascade(label=_('Help'), menu=help_menu)
 root.config(menu=main_menu)
 
@@ -192,16 +289,16 @@ root.columnconfigure(index=4, weight=3)
 for r in range(6): root.rowconfigure(index=r, weight=1)
 
 meta_inf_msg = tk.Message(text=_('Select a .dsp file\n'), width = 384)
-meta_inf_msg.grid(row=0, column=0, columnspan=5, sticky="w", padx = 10)
+meta_inf_msg.grid(row=0, column=0, columnspan=5, sticky='w', padx = 10)
 
 find_peaks_btn = tk.Button(root, text=_('Find peaks'), command=find_peaks, state=tk.DISABLED)
-find_peaks_btn.grid(row=1, column=0, sticky="w", padx = 10)
+find_peaks_btn.grid(row=1, column=0, sticky='w', padx = 10)
 
 remove_peak_btn = tk.Button(root, text=_('Remove peak'), command=remove_peak, state=tk.DISABLED)
-remove_peak_btn.grid(row=1, column=4, sticky="e", padx = 10)
+remove_peak_btn.grid(row=1, column=4, sticky='e', padx = 10)
 
 peaks_listbox = tk.Listbox(root, height=5)
-peaks_listbox.grid(row=2, column=0, columnspan=5, sticky="ew", padx = 10)
+peaks_listbox.grid(row=2, column=0, columnspan=5, sticky='ew', padx = 10)
 
 concentration_label_1 = tk.Label(text=_('Molar concentration:'))
 concentration_label_1.grid(row=3, column=0)
@@ -218,16 +315,16 @@ concentration_exp_entry = ttk.Entry(width=3, textvariable=conc_exp_stringvar)
 concentration_exp_entry.grid(row=3, column=3)
 
 concentration_label_3 = tk.Label(text=_('mol/L'))
-concentration_label_3.grid(row=3, column=4, sticky="w", padx = 10)
+concentration_label_3.grid(row=3, column=4, sticky='w')
 
 calculate_extinction_btn = tk.Button(root, text=_('Calculate extinction'), command=calculate_extinction,  state=tk.DISABLED)
-calculate_extinction_btn.grid(row=4, column=0, sticky="w", padx = 10)
+calculate_extinction_btn.grid(row=4, column=0, sticky='w', padx = 10)
 
 extinction_text_stringvar = tk.StringVar()
 extinction_text_output = ttk.Entry(textvariable=extinction_text_stringvar)
-extinction_text_output.grid(row=5, column=0, columnspan=4, sticky="ew", padx = 10)
+extinction_text_output.grid(row=5, column=0, columnspan=4, sticky='ew', padx = 10)
 
 copy_btn = tk.Button(root, text=_('Copy'), command=copy_extinction_text)
-copy_btn.grid(row=5, column=4, sticky="e", padx = 10)
+copy_btn.grid(row=5, column=4, sticky='e', padx = 10)
 
 root.mainloop()
